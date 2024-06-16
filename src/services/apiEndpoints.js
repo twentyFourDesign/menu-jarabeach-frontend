5170d842e25110fd4258c03dab20d87996385452
k.gujjar@prioticket.com <k.gujjar@prioticket.com>
2024-06-16 23:00:34 +0530

    replace endpoint

--- src/services/apiEndpoints.js
+++ src/services/apiEndpoints.js
@@ -1,7 +1,7 @@
 export const apiEndpoints = {
   ITEMS_LISTING_FILTERED: '/api/v1/items/filtered',
   ITEMS_LISTING_UNFILTERED: '/api/v1/items',
-  CATEGORIES_LISTING: '/api/v1/category',
+  CATEGORIES_LISTING: 'http://64.23.195.110:8081/api/menu/listCategories',
   CREATE_CATEGORY: '/api/v1/category',
   CREATE_ITEM: '/api/v1/items',
   DELETE_CATEGORY: id => `/api/v1/category/${id}`,
