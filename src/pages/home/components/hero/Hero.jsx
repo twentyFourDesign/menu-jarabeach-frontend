773c11160f3fddf5ab0126d7956ee586e1572e9b
Ali Zaid <alizaidkhoso@gmail.com>
2025-06-26 21:39:24 +0500

    header image changed

--- src/pages/home/components/hero/Hero.jsx
+++ src/pages/home/components/hero/Hero.jsx
@@ -1,9 +1,9 @@
-import hero from '../../../../assets/images/header-hero.png';
+import hero from '../../../../assets/images/header-hero.jpg';
 
 export default function Hero() {
   return (
     <div>
-      <img src={hero} className='object-cover' />
+      <img src={hero} alt='jara terrace menu' className='w-full' />
     </div>
   );
 }
